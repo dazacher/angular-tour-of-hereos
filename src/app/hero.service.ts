c831dba0769b0a896add76a9a0295627377d4fc8
dazacher <dazacher2000@yahoo.com>
2021-06-25 09:01:57 -0700

    Added an addHero functionality.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -86,4 +86,12 @@ getHero(id: number): Observable<Hero> {
   httpOptions = {
     headers: new HttpHeaders({ 'Content-Type': 'application/json' })
   };
+
+  /** POST: add a new hero to the server */
+  addHero(hero: Hero): Observable<Hero> {
+    return this.http.post<Hero>(this.heroesUrl, hero, this.httpOptions).pipe(
+      tap((newHero: Hero) => this.log(`added hero w/ id=${newHero.id}`)),
+      catchError(this.handleError<Hero>('addHero'))
+    );
+  }
 }
